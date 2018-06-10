c07791d9a579cec9482bf46e41781d5fa3841acc
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-10 16:39:53 -0400

    Working on Primitive Types and expressions

---
+++ TypeConversionDemo/TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //implicit type conversion
+            byte b = 1;
+            int i = b;
+            Console.WriteLine( i);*/
+
+
+            //explicit type conversion
+            int i = 1;
+            byte b = i;
+
+
+        }
+    }
+}
