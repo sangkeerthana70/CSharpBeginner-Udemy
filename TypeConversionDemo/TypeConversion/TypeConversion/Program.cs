29154b95708fc15000bf6cbe0775d5ce9e07964d
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-10 17:04:56 -0400

    Demo on Type conversions added

--- TypeConversionDemo/TypeConversion/TypeConversion/Program.cs
+++ TypeConversionDemo/TypeConversion/TypeConversion/Program.cs
@@ -12,10 +12,30 @@ namespace TypeConversion
             int i = b;
             Console.WriteLine( i);*/
 
-
+            /*
             //explicit type conversion
-            int i = 1;
-            byte b = i;
+            int i = 1000;
+            byte b = (byte) i; //cast the integer to a byte
+            Console.WriteLine(b);*/
+
+            //non-compatible data type conversion.
+            //Example:1
+            try
+            {
+                var number = "1234";
+                // int i = Convert.ToInt32(number);
+                byte b = Convert.ToByte(number);//this gives handling exception error when the program is run hence the try catch Exception is done
+                Console.WriteLine(b);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+                
+            }
+
+         
+
 
 
         }
