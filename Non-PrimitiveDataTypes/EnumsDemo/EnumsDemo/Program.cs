bedce722e8ae20c225c107a52ea225948cc7e23e
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-13 23:21:39 -0400

    Learning enumerations

---
+++ Non-PrimitiveDataTypes/EnumsDemo/EnumsDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsDemo
+{
+    public enum ShippingMethod
+    {
+        //members
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+        }
+    }
+}
