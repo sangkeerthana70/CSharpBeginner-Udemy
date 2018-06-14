587fbe0d397b089d5ac1cdf64c359a92b1852758
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-14 14:56:21 -0400

    Enum Demo complete

--- Non-PrimitiveDataTypes/EnumsDemo/EnumsDemo/Program.cs
+++ Non-PrimitiveDataTypes/EnumsDemo/EnumsDemo/Program.cs
@@ -26,9 +26,14 @@ namespace EnumsDemo
             Console.WriteLine(method.ToString());
 
             //EX-2 convert a string to enum if stored in a database or some other system
-
-
-
+            var methodName = "Express"; //convert this to a shippingMethod enum
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            /*1-first parameter is the  type which is the ShippingMethod to which we want the string to be converted into,
+            2-second parameter is the string value for which we give the methodName.
+            3-the return type returns an object, it does not return a shippingMethod, to do this we have to explicitly cast 
+            4-the object that is returned to a ShippingMethod(enum).
+            5-finally store the result of this expression in a separate variable called shippingMethod*/
+            Console.WriteLine(shippingMethod);
         }
     }
 }
