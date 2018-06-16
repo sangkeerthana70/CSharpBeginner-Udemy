2f0a211c23a6df00aee56fd59510bed288147db2
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-16 08:41:10 -0400

    Demo on Reference and Value types done

---
+++ Non-PrimitiveDataTypes/ReferenceTypesAndValueTypes/ExampleReferenceAndValueType/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleReferenceAndValueType
+{
+    //class Person
+    public class Person
+    {
+        public int Age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex:1 passing by value type
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);//value will be 1 unchanged as passed by value 
+
+            //Ex:2 passing by reference type
+            var person = new Person() {Age = 20};
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        
+        //static methods that are void wont return any value but created to call them without creating an object(demo purposes)
+         
+        public static void Increment(int number)
+        {
+            //addition assignment operator in action
+            number += 10;
+        }
+        
+        public  static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
+    }
+}
