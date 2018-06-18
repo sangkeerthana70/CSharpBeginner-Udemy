a22ffdfc3adaa3bbec1ea5bf62aeffcd53eb3fb1
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-18 17:48:01 -0400

    Control flow done

--- ControlFlow/RandomClass/RandomClass/Program.cs
+++ ControlFlow/RandomClass/RandomClass/Program.cs
@@ -25,16 +25,33 @@ namespace RandomClass
             */
 
             //Example-2 Random class to generate random string
-            
+            /*
             for (var i = 0; i < 10; i++)
-            {
-                var randomChar = random.Next(97, 122);//cast the number generated to character
+            
+                var randomChar = random.Next(97, 122);//cast the number generated to character since 97 to 122 represent ascii chars from a to z 
                 Console.Write((char)randomChar);//Console.Write is used to print the output in the same line
                 Console.WriteLine();
+            
+            */
+
+            //Ex-3
+            for (var i = 0; i < 10; i++)
+            
+                Console.Write((char)('a' + random.Next(0, 26)));
+                Console.WriteLine();
+
+
+            //EX-4 display the string after storing it in a array instead of using console.writeline
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++)
+            {
+                buffer[i] = (char)('a' + random.Next(0, 26));
             }
-
-
-
+            //create string based on the char array using the string class
+            var password = new string(buffer);
+            Console.WriteLine(password);
+            
         }
     }
 }
