0c26ef977939e75e4db6ad395444b0e044e7bcbd
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-21 13:14:13 -0400

    Demo on Lists

--- DemoLists/DemoLists/Program.cs
+++ DemoLists/DemoLists/Program.cs
@@ -16,9 +16,22 @@ namespace DemoLists
             numbers.Add(7);//add one object to a list
             
             //add another list or another array to a list
-            numbers.AddRange(new int[3] { 10, 11, 12 });
+            numbers.AddRange(new int[4] { 10, 11, 12, 1 });
             foreach(var i in numbers)
                 Console.WriteLine(i);
+            Console.WriteLine();//print an empty line
+            Console.WriteLine("Index of 7: " + numbers.IndexOf(7));//returns the position of the number in the List
+            //Last index of method to locate the last index of duplicate elements in the lists.
+            Console.WriteLine("Last Index of 1: " + numbers.LastIndexOf(1));
+
+            //count 
+            Console.WriteLine("Count: " + numbers.Count);
+
+            //Remove() method
+            numbers.Remove(1);
+            foreach(var j in numbers)
+                Console.WriteLine(j);
+           
         }
     }
 }
