5b5d47cfa25caf86435825e86d7c67a1b2104ef2
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-28 07:43:46 -0400

    Time demo w.i.p

--- WorkingWithDates/timeSpanDemo/timeSpanDemo/Program.cs
+++ WorkingWithDates/timeSpanDemo/timeSpanDemo/Program.cs
@@ -10,6 +10,17 @@ namespace timeSpanDemo
     {
         static void Main(string[] args)
         {
+            var timeSpan = new TimeSpan(1, 2, 3);
+
+
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Time duration: " + duration);
+
         }
     }
 }
