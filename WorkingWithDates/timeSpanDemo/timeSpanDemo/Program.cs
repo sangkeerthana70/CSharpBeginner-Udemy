8ae2f03f13ec6271f3eae5e5f1f8e3c441747574
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-28 08:07:10 -0400

    TimeSpan demo

--- WorkingWithDates/timeSpanDemo/timeSpanDemo/Program.cs
+++ WorkingWithDates/timeSpanDemo/timeSpanDemo/Program.cs
@@ -10,6 +10,7 @@ namespace timeSpanDemo
     {
         static void Main(string[] args)
         {
+            //creating timeSPan objects
             var timeSpan = new TimeSpan(1, 2, 3);
 
 
@@ -20,6 +21,18 @@ namespace timeSpanDemo
             var end = DateTime.Now.AddMinutes(2);
             var duration = end - start;
             Console.WriteLine("Time duration: " + duration);
+            //properties of timeSpan class
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+            //Add and Subtract examples
+            Console.WriteLine("Add example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Subtract example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
+
+            //Convert timeSpan to string  using ToString method
+            Console.WriteLine("Convert timeSpan to String: " + timeSpan.ToString());
+            //parse string to timeSpan
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
+            
 
         }
     }
