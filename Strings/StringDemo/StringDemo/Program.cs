e303b39f4f0e7244fc6384645f41dadc06532c29
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-29 07:41:06 -0400

    Working with strings

---
+++ Strings/StringDemo/StringDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myFullName = " Elizabeth Warren ";
+            Console.WriteLine("Trim: '{0}'", myFullName.Trim());
+            Console.WriteLine("ToUpper:  '{0}'", myFullName.Trim().ToUpper());
+        }
+    }
+}
