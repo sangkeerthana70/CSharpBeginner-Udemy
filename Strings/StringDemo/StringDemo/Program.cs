6ef5bcabbfd5a4b1938a0872483a85e5808d2ed2
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-29 08:04:53 -0400

    Demo on Strings

--- Strings/StringDemo/StringDemo/Program.cs
+++ Strings/StringDemo/StringDemo/Program.cs
@@ -10,9 +10,18 @@ namespace StringDemo
     {
         static void Main(string[] args)
         {
-            var myFullName = " Elizabeth Warren ";
+            var myFullName = "Elizabeth Warren ";
             Console.WriteLine("Trim: '{0}'", myFullName.Trim());
             Console.WriteLine("ToUpper:  '{0}'", myFullName.Trim().ToUpper());
+            //break the string into first and last name
+            //Use of string.IndexOf() and substring() methods
+            var index = myFullName.IndexOf(' ');//returns the indexof upto a space 
+            var firstName = myFullName.Substring(0, index);
+            var lastName = myFullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
+
         }
     }
 }
