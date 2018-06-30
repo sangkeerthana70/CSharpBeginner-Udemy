110e8cdccf38a730482e3bc4c0c99ff70d2ab145
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-29 23:35:21 -0400

    Strings Demo complete

--- Strings/StringDemo/StringDemo/Program.cs
+++ Strings/StringDemo/StringDemo/Program.cs
@@ -20,7 +20,36 @@ namespace StringDemo
             var lastName = myFullName.Substring(index + 1);
             Console.WriteLine("FirstName: " + firstName);
             Console.WriteLine("LastName: " + lastName);
+            //split string using string.split() method
+            var names = myFullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
+            //using replace method to replace a string 
+            var replace = myFullName.Replace("Elizabeth", "ElizabethAnne");
+            var replace1 = myFullName.Replace("a", "A");
 
+            Console.WriteLine(replace);
+            Console.WriteLine(replace1);
+            /*
+             * //String.IsNullOrEmpty method
+            if (String.IsNullOrEmpty(" ".Trim()))
+            {
+                Console.WriteLine("Invalid");
+            }*/
+
+            //above method replaced by IsNullOrWhiteSpace method to make empty user input invalid.
+            if (String.IsNullOrWhiteSpace(" "))
+                Console.WriteLine("Invalid");
+
+            //convert string to  numbers 
+            var str = "25";
+            var age = Convert.ToByte(str);//convert the str to a byte instead of Int32(which takes 4 bytes) since a byte can hold upto int-250
+            Console.WriteLine(age);
+            //convert number to string
+            float price = 29.95f;
+            //convert the var price from float to a string representation of a number 
+            //Console.WriteLine(price.ToString("C"));//which is then formatted to currency using format string
+            Console.WriteLine(price.ToString("C0"));//C0 formats it to a whole number by rounding it and getting rid of the decimal points
 
         }
     }
