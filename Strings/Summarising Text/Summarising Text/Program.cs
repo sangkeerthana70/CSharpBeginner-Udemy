c00839e2c98b84ebad0e9cc569534a7f4f920b35
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-30 15:38:03 -0400

    Working on Summarizing text

---
+++ Strings/Summarising Text/Summarising Text/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summarising_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really long text";
+            const int maxLength = 20;
+
+            if (sentence.Length = maxLength)
+            {
+                Console.WriteLine(sentence);
+            }
+            else
+            {
+                //summarize the sentence using substring method()
+                sentence.Substring(0, maxLength);//this might omit some words from sentence
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach(var word in words)
+                {
+                    totalCharacters += word.Length;
+                    if (totalCharacters > maxLength)
+                        break;
+                }
+
+            }
+        }
+    }
+}
