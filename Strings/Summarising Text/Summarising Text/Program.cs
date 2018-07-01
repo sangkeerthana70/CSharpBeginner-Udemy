5a3afec0d2d3db259c8ed6f0aa5b6cce348d08f8
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-07-01 13:33:03 -0400

    Summarising Text

--- Strings/Summarising Text/Summarising Text/Program.cs
+++ Strings/Summarising Text/Summarising Text/Program.cs
@@ -13,24 +13,30 @@ namespace Summarising_Text
             var sentence = "This is going to be a really really really really really long text";
             const int maxLength = 20;
 
-            if (sentence.Length = maxLength)
+            if (sentence.Length < maxLength)
             {
                 Console.WriteLine(sentence);
             }
             else
             {
                 //summarize the sentence using substring method()
-                sentence.Substring(0, maxLength);//this might omit some words from sentence
-                var words = sentence.Split(' ');
-                var totalCharacters = 0;
-                var summaryWords = new List<string>();
+                //sentence.Substring(0, maxLength);//this might omit some words from sentence
 
-                foreach(var word in words)
+                //split sentence into words using space as a delimiter
+                var words = sentence.Split(' ');//creates a string array of words
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();//stores words dynamically as the summary is created
+
+                foreach(var word in words)//loop over the words
                 {
-                    totalCharacters += word.Length;
+                    summaryWords.Add(word);
+                    totalCharacters += word.Length + 1;//get the length of each word and add it to the totalCharacter variable. +1 indicates the space after each word.
+                    
                     if (totalCharacters > maxLength)
-                        break;
+                        break;//break out of loop if totalCharacters exceeds 20.
                 }
+                var summary = String.Join(" ", summaryWords) + "...";//join with space as a delimiter
+                Console.WriteLine(summary);
 
             }
         }
