bdc7347fb30866596216171a3a26a6a69601ee7e
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-07-07 12:41:29 -0400

    Started Procedural Programming lecture

---
+++ Strings/ProceduralProgramming/ProceduralProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? ");
+            var name = Console.ReadLine();
+
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+            var reversed = new string(array);
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+    }
+}
