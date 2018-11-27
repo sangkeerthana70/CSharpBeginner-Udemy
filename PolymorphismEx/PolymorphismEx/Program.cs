1dc45fa95eb85a3e7e212263bf04dc50ee4bcf93
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-27 17:54:39 -0500

    Brushing up on C# basics

---
+++ PolymorphismEx/PolymorphismEx/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullDozer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new FactoryWorker();
+            worker.Count(10);
+            worker.Count(25);
+        }
+    }
+
+    public class FactoryWorker
+    {
+        //property
+        public int total { get; set; }
+        //constructor
+        public FactoryWorker()
+        {
+            
+        }
+
+        // method
+        public void Count(int number)
+        {
+            if (total == null)
+                total = 0;
+            total = total + number;
+            Console.WriteLine(total.ToString());
+
+
+        }
+    }
+}
